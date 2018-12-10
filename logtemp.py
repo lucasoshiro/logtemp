c969ea3eb8db86669a8ca31bd82564bd4e79754b
Lucas Oshiro <lucasseikioshiro@gmail.com>
2018-12-10 19:31:57 -0200

    Initial Commit

---
+++ logtemp.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+from sys import argv, stderr
+from time import sleep
+from datetime import datetime
+import subprocess
+import json
+import re
+import os
+
+def get_temp():
+    cmd = 'vcgencmd measure_temp'
+    temp_regex = 'temp=([0-9.]+)\'C'
+    p_cmd = os.popen(cmd)
+    temp = float(re.match(temp_regex, p_cmd.read()).groups()[0])
+    p_cmd.close()
+    return temp
+
+def sample(n, workers):
+    stderr.write('{} {}\n'.format(n, workers))
+    def _single_sample():
+        s = {
+            'timestamp':   str(datetime.now()),
+            'temperature': get_temp(),
+            'workers':     workers
+        }
+        sleep(1)
+        return s
+
+    stress = subprocess.Popen(
+        ['stress', '-c', str(workers)], stdout=open(os.devnull, 'wb')) if workers > 0 else None
+    results = [_single_sample() for i in range(n)]
+    if stress: stress.kill()
+    return results
+
+def main():
+    sample_0_4 = dict((i, sample(20, 0) + sample(40, i) + sample(600, 0)) for i in range(5))
+    print(json.dumps(sample_0_4))
+
+if __name__ == '__main__':
+    main()
